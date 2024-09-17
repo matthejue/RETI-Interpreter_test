92f215635ec8dae17f85d0d168a48f71b9a3f0f5
areotwister <areotwister@gmail.com>
2024-09-18 01:15:34 +0200

    fixed error with multiple definitions for const variables

--- include/globals.h
+++ include/globals.h
@@ -7,9 +7,9 @@ extern uint32_t ram_size;
 extern uint16_t page_size;
 extern uint32_t hdd_size;
 
-const char NUM_REGISTERS = 8;
-const short NUM_INSTRUCTIONS_START_PROGRAM = 16;
-const char NUM_UART_ADDRESSES = 8;
+#define NUM_REGISTERS 8
+#define NUM_INSTRUCTIONS_START_PROGRAM 16
+#define NUM_UART_ADDRESSES 8
 
 
 #endif // GLOBALS_H
