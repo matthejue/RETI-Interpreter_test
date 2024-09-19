15ee5e6fca21c1e464ac4608e4625447dc4fe35a
areotwister <areotwister@gmail.com>
2024-09-19 16:11:34 +0200

    added derivation of mod function that behevaes like math modulo

--- src/interpret.c
+++ src/interpret.c
@@ -3,6 +3,23 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+// r = a % m <-> r + m * q = a + m * p <-> r = a - m * q (q is biggest q such that q*m <= a) = a − m * (a / m)
+// 0 <= r < m, a,q € Z, m € N/0 (normal C / hardware implementation ignores 0 <= r < m)
+// 7 % 3 = 7 - 3 * (7 / 3) = 1
+// -7 % 3 = -7 - 3 * (-7 / 3) = -1, but r has to be positive, get next congruent element of congruence class by -1 + 3 = 2 (one can easily get negative or positive r with -m < r < m by just nr = r - m and also r = nr + m)
+// irrelevant cases where on ignores m € N/0:
+// 7 % -3 = 7 - (-3) * (7 / -3) = 1 (if we ignore m € N/0), but that is only for truncated division, for floor it is 7 - (-3) * (7 / -3) = 7 - (-3) * -3 = 7 - 9 = -2 (if we ignore 0 <= r < m and say -m < r < m)
+// -7 % -3 = -7 - (-3) * (-7 / -3) = -1 (if we ignore m € N/0 and 0 =< r < m and thus -m < r < m), for floor it is the same: -7 - (-3) * (-7 / -3) = -7 - (-3) * 2 = -7 - (-6) = -1, -1 + 3 = 2 (if don't want to also ignore 0 =< r < m)
+
+int mod(int a, int b) {
+  // r = a − m x trunc(a/m)
+  int result = a % b;
+  if (result < 0) {
+    result += b;
+  }
+  return result;
+}
+
 void interpr_instruction(Instruction *assembly_instr) {
   switch (assembly_instr->op) {
   case ADDI:
@@ -27,8 +44,8 @@ void interpr_instruction(Instruction *assembly_instr) {
     break;
   case MODI:
     write_storage(regs, assembly_instr->opd1,
-                  (int32_t)read_storage(regs, assembly_instr->opd1) %
-                      (int32_t)assembly_instr->opd2);
+                  mod((int32_t)read_storage(regs, assembly_instr->opd1),
+                      (int32_t)assembly_instr->opd2));
     break;
   case OPLUSI:
     write_storage(regs, assembly_instr->opd1,
@@ -67,8 +84,8 @@ void interpr_instruction(Instruction *assembly_instr) {
     break;
   case MODR:
     write_storage(regs, assembly_instr->opd1,
-                  (int32_t)read_storage(regs, assembly_instr->opd1) %
-                      (int32_t)read_storage(regs, assembly_instr->opd2));
+                  mod((int32_t)read_storage(regs, assembly_instr->opd1),
+                      (int32_t)read_storage(regs, assembly_instr->opd2)));
     break;
   case OPLUSR:
     write_storage(regs, assembly_instr->opd1,
@@ -107,8 +124,8 @@ void interpr_instruction(Instruction *assembly_instr) {
     break;
   case MODM:
     write_storage(regs, assembly_instr->opd1,
-                  (int32_t)read_storage(regs, assembly_instr->opd1) %
-                      (int32_t)read_file(sram, assembly_instr->opd2));
+                  mod((int32_t)read_storage(regs, assembly_instr->opd1),
+                      (int32_t)read_file(sram, assembly_instr->opd2)));
     break;
   case OPLUSM:
     write_storage(regs, assembly_instr->opd1,
